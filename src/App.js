5e984bbdd9efe4e28c6f219b8042685b58db1c68
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-01-21 08:19:26 -0800

    Search Engine set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
+
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h2>
-         Dictionary
-       </h2>
+       <Dictionary />
       </header>
+      <footer className="App-footer">
+        <small> 
+          Coded by Thais Damasceno, open-source on GitHub and hosted on Netifly
+        </small>
+      </footer>
     </div>
   );
 }
