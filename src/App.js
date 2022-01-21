a61c77ad7b2d0f862d68aca737d2553be725f561
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-01-20 18:24:18 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
