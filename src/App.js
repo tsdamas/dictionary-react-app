ef1cd019e2745a3aed56c94698c74d9ad9e85911
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-01-21 18:56:21 -0800

    API integration

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
+        <h1>Dictionary</h1>
       </header>
       <main>
        <Dictionary />
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,24 @@
 import React, {useState} from "react";
-import "./Dictionary.css"
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary(){
   
   let [keyword, setKeyword] = useState(""); 
 
+  function handleResponse(response){
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching ${keyword}` );
+    alert(`Searching ${keyword}`);
+
+    let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+
   }
+
 
   function handleKeywordChange (event){
    setKeyword(event.target.value);
