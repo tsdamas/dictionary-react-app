afcc0a726e5232aab1a25239a8f90b31af9643ab
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-01-21 18:25:56 -0800

    Improved set up

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-       <Dictionary />
       </header>
+      <main>
+       <Dictionary />
+      </main>
       <footer className="App-footer">
         <small> 
           Coded by Thais Damasceno, open-source on GitHub and hosted on Netifly
         </small>
       </footer>
+      </div>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,7 +7,7 @@ export default function Dictionary(){
 
   function search(event) {
     event.preventDefault();
-    alert("Searching");
+    alert(`Searching ${keyword}` );
   }
 
   function handleKeywordChange (event){
