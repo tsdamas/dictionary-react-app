c5a347f386ebff5f9bb98ec7f862586ebd6260c9
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-01-23 14:02:17 -0800

    Added results component
    - Added Meaning component;
    - Began style;

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,21 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary(){
   
   let [keyword, setKeyword] = useState(""); 
+  let [results, setResults] = useState(null);
 
   function handleResponse(response){
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching ${keyword}`);
-
+    
+    //documentation:https://dictionaryapi.dev
     let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
 
@@ -27,8 +29,9 @@ export default function Dictionary(){
   return(
     <div className="Dictionary">
      <form onSubmit={search}>
-       <input type="search" autoFocus={true} onChange={handleKeywordChange} /> 
+       <input type="search" placeholder="Search word" autoFocus={true} onChange={handleKeywordChange} /> 
      </form>
+     <Results results={results} />
     </div>
   )
 }
