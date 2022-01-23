df4c3bbe4a042c5062978a9403dd25bda67b59bd
Thais Damasceno <85142451+tsdamas@users.noreply.github.com>
2022-01-23 14:49:50 -0800

    Started style structure

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,29 @@
 import React from "react";
+import "./Meaning.css"; 
 
 export default function Meaning(props){
   return (
     <div className="Meaning">
-    <h3>{props.meaning.partOfSpeech}</h3>
+    <h5>{props.meaning.partOfSpeech}</h5>
     {props.meaning.definitions.map(function(definition, index){
       return (
-        <div key={index}>
+    <div key={index}>
+      <div className="col-sm-9">
+        <div className="row">
+          <div class="col-8 col-sm-6">
           <p>
             {definition.definition}
           </p>
+            </div>
           <br />
+          <div class="col-4 col-sm-6">
           <p>
             <em>{definition.example}</em>
           </p>
-        </div>
+              </div>
+            </div>
+          </div>
+      </div>
       );
     })}
   </div>
--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,13 @@ export default function Results(props){
 if (props.results) {
   return(
   <div className="results">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-3">
     <h2>{props.results.word}</h2>
+    <h4>Definition</h4>
+    <hr />
+    </div>
     {props.results.meanings.map(function(meaning, index) {
       return (
         <div key={index}>
@@ -14,6 +20,8 @@ if (props.results) {
         </div>   
       );
     })}
+      </div>
+    </div>
   </div>
   );
 } else {
